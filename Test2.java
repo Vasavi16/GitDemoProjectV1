c3fdd095d6cf18225108bcaadbd381b3fc1c70bd
Vasavi16 <vasavi7531@gmail.com>
2021-12-06 21:48:50 +0530

    test2 after modifications

--- Test2.java
+++ Test2.java
@@ -3,7 +3,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 String angry="yes";
-if(angry=="yes" || angry=="y")
+if(angry=="yes" || angry=="y" || angry=="Y")
 	System.out.println("say something");
 else
 	System.out.println("do work");
