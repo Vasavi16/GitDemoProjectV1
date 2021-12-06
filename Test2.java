b7c4cdc125823002fa2bbc16f0175d98e881bfeb
Vasavi16 <vasavi7531@gmail.com>
2021-12-06 21:42:32 +0530

    commiting test2 after review

--- Test2.java
+++ Test2.java
@@ -3,7 +3,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 String angry="yes";
-if(angry=="yes")
+if(angry=="yes" || angry=="y")
 	System.out.println("say something");
 else
 	System.out.println("do work");
